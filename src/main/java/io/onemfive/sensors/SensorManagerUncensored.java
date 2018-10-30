ad80df4906d414282d1b19cfaa2b7c8738af0cd7
objectorange <objectorange@protonmail.com>
2018-10-30 00:46:49 -0400

    begin buildig censorship-resistant logic

---
+++ src/main/java/io/onemfive/sensors/SensorManagerUncensored.java
@@ -0,0 +1,96 @@
+package io.onemfive.sensors;
+
+import io.onemfive.data.Envelope;
+
+/**
+ * Sensitivity order from least to greatest is defined in Envelope.Sensitivity with default protocols:
+ *
+ * NONE: HTTP
+ * LOW: HTTPS
+ * MEDIUM: Tor
+ * HIGH: I2P
+ * VERYHIGH: I2P Bote
+ * EXTREME: Mesh
+ * NEO: A combination of all anonymous networks from MEDIUM to EXTREME
+ *
+ * We are working towards providing the following sensitivity routing logic:
+ *
+ * ** 1M5 Inter-Node Communications **
+ * All communications between 1M5 peers defaults to I2P unless the Envelope's sensitivity
+ * is set to VERYHIGH which indicates that higher sensitivity is required with
+ * acceptable higher latency or when set to EXTREME and MESH is available.
+ * 1M5's communication foundation starts with I2P as it provides the lowest latency / greatest
+ * privacy trade-off. MESH is not yet available so a Sensitivity of EXTREME will use
+ * I2P Bote with random delays set high.
+ *
+ * ** EXTERNAL COMMUNICATIONS **
+ * All communications specifying an external HTTP URL will:
+ *
+ * * NONE *
+ * Use HTTPS if specified in the URL otherwise HTTP. If HTTPS fails, fall back to HTTP.
+ *
+ * * LOW *
+ * If HTTP supplied in URL, try HTTPS anyways.
+ *
+ * * MEDIUM *
+ * Use Tor to reach specified HTTP/HTTPS URL.
+ *
+ * * HIGH *
+ * If HTTP/HTTPS URL specified, use peers through I2P to reach a peer that can successfully use Tor.
+ *
+ * * VERYHIGH *
+ * If HTTP/HTTPS URL specified, use peers through I2P Bote to reach a peer that can successfully use Tor.
+ *
+ * * EXTREME *
+ * If HTTP/HTTPS URL specified and MESH not available, use peers through I2P Bote with high random delays
+ * to reach a peer that can successfully use Tor. If MESH is available, use that instead.
+ *
+ * * GENERAL PEER PROPAGATION *
+ * 1. If any of the above fails, send request to another peer via I2P to have it attempt it.
+ * 2. If the protocol specified fails at the peer, it will forward onto randomly chosen
+ * (likely to get smarter in future) next peer and retry.
+ * 3. This will occur for specified number of attempts up to a maximum 10 until tokenization is implemented
+ * at which it will continue until supplied tokens for transaction are exhausted.
+ * 4. If I2P fails during any of these attempts and MESH is available, MESH will take over.
+ *
+ * The SensorManagerNeo4j is more complex using the Neo4j Graph database embedded.
+ * The 1M5 Neo4j library must be included to use this.
+ *
+ * @author objectorange
+ */
+public class SensorManagerUncensored extends SensorManagerSimple {
+
+    @Override
+    public Sensor selectSensor(Envelope e) {
+        return super.selectSensor(e);
+//        Sensor s = null;
+//        if(e.getRoute() != null && e.getURL() != null) {
+//            String p = e.getURL().getPath();
+//            if(p.startsWith("http")) {
+//                if(p.startsWith("https")) {
+//
+//                } else {
+//                    // Only HTTPS specified
+//                    switch(e.getSensitivity()) {
+//                        case LOW: {
+//                            // try https anyways
+//                            break;
+//                        }
+//                        case MEDIUM: {
+//                            // use Tor if available
+//
+//                            break;
+//                        }
+//                        case NONE:
+//                        default: {
+//                            // try http
+//
+//                        }
+//                    }
+//                }
+//            }
+//        }
+//
+//        return s;
+    }
+}
