1ae04c6dcb0dfb09b874543fd3e9c218c7331acd
objectorange <objectorange@protonmail.com>
2019-07-04 20:41:15 -0400

    additional methods to fill out Sensor requirements

--- src/main/java/io/onemfive/sensors/Sensor.java
+++ src/main/java/io/onemfive/sensors/Sensor.java
@@ -3,24 +3,29 @@ package io.onemfive.sensors;
 import io.onemfive.core.LifeCycle;
 import io.onemfive.data.Envelope;
 import io.onemfive.data.NetworkPeer;
+import io.onemfive.sensors.peers.PeerManager;
 
 import java.util.Map;
 
 /**
- * Interface for all Sensors.
+ * Expected behavior from a Sensor.
  *
  * @author objectorange
  */
 public interface Sensor extends LifeCycle {
     boolean send(Envelope envelope);
     boolean reply(Envelope envelope);
+    void setNetwork(NetworkPeer.Network network);
+    NetworkPeer.Network getNetwork();
     SensorStatus getStatus();
     Integer getRestartAttempts();
-    Map<String,NetworkPeer> getPeers();
     Envelope.Sensitivity getSensitivity();
     String[] getOperationEndsWith();
     String[] getURLBeginsWith();
     String[] getURLEndsWith();
-    // When Sensitivities are equal, priority determines order of evaluation
+    // When Sensitivities are equal, priority can factor into order of evaluation
     Integer getPriority();
+    void setSensorManager(SensorManager sensorManager);
+    void setSensitivity(Envelope.Sensitivity sensitivity);
+    void setPriority(Integer priority);
 }
