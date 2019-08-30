69249deba7f0721ebe79ac15b87e59bd090d0126
objectorange <objectorange@1m5.io>
2019-08-29 22:36:04 -0400

    add mancon descriptions; separate selected sensors - more logic to come

--- src/main/java/io/onemfive/sensors/SensorManagerUncensored.java
+++ src/main/java/io/onemfive/sensors/SensorManagerUncensored.java
@@ -12,7 +12,7 @@ import java.util.logging.Logger;
  * MEDIUM: Tor
  * HIGH: I2P
  * VERYHIGH: I2P with High Delays
- * EXTREME: Direct Ad-Hoc Network
+ * EXTREME: 1DN Wireless Direct Ad-Hoc Network
  * NEO: A combination of all anonymous networks from MEDIUM to EXTREME
  *
  * We are working towards providing the following sensitivity routing logic:
@@ -28,37 +28,35 @@ import java.util.logging.Logger;
  * ** EXTERNAL COMMUNICATIONS **
  * All communications specifying an external HTTP URL will:
  *
- * * NONE *
+ * * NONE - MANCON 6*
  * Use HTTPS if specified in the URL otherwise HTTP. If HTTPS fails, fall back to HTTP.
- * This should only be used for localhost.
  *
- * * LOW *
+ * * LOW - MANCON 5 *
  * If HTTP supplied in URL, try HTTPS anyways. If HTTPS doesn't work, provide and/or log a warning.
  * This may be ok for accessing little known web services where privacy is not a concern.
  *
- * * MEDIUM *
+ * * MEDIUM - MANCON 4 *
  * Use Tor to reach specified HTTP/HTTPS URL.
  *
- * * HIGH *
+ * * HIGH - MANCON 3 *
  * If HTTP/HTTPS URL specified, use peers through I2P to reach a peer that can successfully use Tor.
  *
- * * VERYHIGH *
+ * * VERYHIGH - MANCON 2 *
  * If HTTP/HTTPS URL specified, use peers through I2P with high random delays to reach a peer that can successfully use Tor.
  *
- * * EXTREME *
+ * * EXTREME - MANCON 1 *
  * If HTTP/HTTPS URL specified and Direct Mesh not available, use peers through I2P with the max high random delays
  * to reach a peer that can successfully use Tor. If Direct Mesh is available, use that instead.
  *
+ * * NEO - MANCON 0 *
+ * Use a combination of anonymity networks to maximize anonymity.
+ *
  * * GENERAL PEER PROPAGATION *
- * 1. If any of the above fails, send request to another peer via I2P to have it attempt it.
+ * 1. If any of the above fails, send request to another peer to have it attempt it using an escalated network.
  * 2. If the protocol specified fails at the peer, it will forward onto randomly chosen
  * (likely to get smarter in future) next peer and retry.
  * 3. This will occur for specified number of attempts up to a maximum 10 until tokenization is implemented
  * at which it will continue until supplied tokens for transaction are exhausted.
- * 4. If I2P fails during any of these attempts and Direct Mesh is available, Direct Mesh will take over.
- *
- * The SensorManagerNeo4j is more complex using the Neo4j Graph database embedded.
- * The 1M5 Neo4j library must be included to use this.
  *
  * @author objectorange
  */
@@ -68,68 +66,59 @@ public class SensorManagerUncensored extends SensorManagerSimple {
 
     @Override
     public Sensor selectSensor(Envelope e) {
-        // Lookup sensor by simple means
-        Sensor s = super.selectSensor(e);
+        // Lookup sensor by simple normal means
+        Sensor simpleSelected = super.selectSensor(e);
+        Sensor selected = simpleSelected;
         String err = null;
-        if(s == null) {
+        if(simpleSelected == null) {
             // Sensor not determined by request - we have a problem
             err = "Unable to select sensor from request. Please ensure Envelope sensitivity, operation, or url is set to a supported Sensor.";
         } else {
             // Sensor determined by request
-            if(blockedSensors.get(s.getClass().getName()) != null
-                    && SensorStatus.NETWORK_CONNECTED.name().equals(s.getStatus().name())) {
-                // Remove from Blocked Sensors List
-                blockedSensors.remove(s.getClass().getName());
-                LOG.info(s.getClass().getSimpleName() + " removed from blocked sensors list.");
-            }
-            switch(s.getStatus()) {
-                case NETWORK_BLOCKED: {
-                    if(TOR_SENSOR_NAME.equals(s.getClass().getName())) {
-                        LOG.info("Tor Sensor blocked.");
-                        // Tor is being blocked, switch to I2P/Radio/LiFi
-                        if(getActiveSensors().get(I2P_SENSOR_NAME) == null) {
-                            if(getActiveSensors().get(RADIO_SENSOR_NAME) == null) {
-                                if(getActiveSensors().get(LIFI_SENSOR_NAME) == null) {
-                                    err = "TOR blocked and I2P, Radio, and LiFi Sensors not active. Please register I2P, Radio, and/or LiFi Sensor to ensure TOR can be re-routed through I2P, Radio, or LiFi when blocked.";
-                                } else {
-                                    LOG.info("LiFi Sensor is active; switching to LiFi...");
-                                    s = getActiveSensors().get(LIFI_SENSOR_NAME);
-                                }
-                            } else {
-                                LOG.info("Radio Sensor is active; switching to Radio...");
-                                s = getActiveSensors().get(RADIO_SENSOR_NAME);
-                            }
-                        } else {
-                            LOG.info("I2P Sensor is active; switching to I2P...");
-                            s = getActiveSensors().get(I2P_SENSOR_NAME);
-                        }
-                    } else if(I2P_SENSOR_NAME.equals(s.getClass().getName())) {
-                        LOG.info("I2P Sensor blocked.");
-                        // I2P is being blocked, switch to Radio/LiFi
+            if(simpleSelected.getStatus() == SensorStatus.NETWORK_BLOCKED) {
+                if(TOR_SENSOR_NAME.equals(simpleSelected.getClass().getName())) {
+                    LOG.info("Tor Sensor blocked.");
+                    // Tor is being blocked, switch to I2P/Radio/LiFi
+                    if(getActiveSensors().get(I2P_SENSOR_NAME) == null) {
                         if(getActiveSensors().get(RADIO_SENSOR_NAME) == null) {
                             if(getActiveSensors().get(LIFI_SENSOR_NAME) == null) {
-                                err = "I2P blocked and Radio nor LiFi Sensors are active. Please register Radio and/or LiFi Sensor to ensure I2P can be re-routed through Radio or LiFI when blocked.";
+                                err = "TOR blocked and I2P, Radio, and LiFi Sensors not active. Please register I2P, Radio, and/or LiFi Sensor to ensure TOR can be re-routed through I2P, Radio, or LiFi when blocked.";
                             } else {
                                 LOG.info("LiFi Sensor is active; switching to LiFi...");
-                                s = getActiveSensors().get(LIFI_SENSOR_NAME);
+                                selected = getActiveSensors().get(LIFI_SENSOR_NAME);
                             }
                         } else {
                             LOG.info("Radio Sensor is active; switching to Radio...");
-                            s = getActiveSensors().get(RADIO_SENSOR_NAME);
+                            selected = getActiveSensors().get(RADIO_SENSOR_NAME);
                         }
-                    } else if(RADIO_SENSOR_NAME.equals(s.getClass().getName())) {
-                        LOG.info("Radio Sensor blocked.");
-                        // Radio is being blocked, switch to LiFi
+                    } else {
+                        LOG.info("I2P Sensor is active; switching to I2P...");
+                        selected = getActiveSensors().get(I2P_SENSOR_NAME);
+                    }
+                } else if(I2P_SENSOR_NAME.equals(simpleSelected.getClass().getName())) {
+                    LOG.info("I2P Sensor blocked.");
+                    // I2P is being blocked, switch to Radio/LiFi
+                    if(getActiveSensors().get(RADIO_SENSOR_NAME) == null) {
                         if(getActiveSensors().get(LIFI_SENSOR_NAME) == null) {
-                            err = "Radio blocked and LiFi Sensor not active. Please register LiFi Sensor to ensure Radio can be re-routed through LiFi when blocked.";
+                            err = "I2P blocked and Radio nor LiFi Sensors are active. Please register Radio and/or LiFi Sensor to ensure I2P can be re-routed through Radio or LiFI when blocked.";
                         } else {
                             LOG.info("LiFi Sensor is active; switching to LiFi...");
-                            s = getActiveSensors().get(LIFI_SENSOR_NAME);
+                            selected = getActiveSensors().get(LIFI_SENSOR_NAME);
                         }
+                    } else {
+                        LOG.info("Radio Sensor is active; switching to Radio...");
+                        selected = getActiveSensors().get(RADIO_SENSOR_NAME);
                     }
-                    break;
+                } else if(RADIO_SENSOR_NAME.equals(simpleSelected.getClass().getName())) {
+                    LOG.info("Radio Sensor blocked.");
+                    // Radio is being blocked, switch to LiFi
+                    if(getActiveSensors().get(LIFI_SENSOR_NAME) == null) {
+                        err = "Radio blocked and LiFi Sensor not active. Please register LiFi Sensor to ensure Radio can be re-routed through LiFi when blocked.";
+                    } else {
+                        LOG.info("LiFi Sensor is active; switching to LiFi...");
+                        selected = getActiveSensors().get(LIFI_SENSOR_NAME);
+                    }
                 }
-
             }
         }
         if(err != null) {
@@ -167,6 +156,6 @@ public class SensorManagerUncensored extends SensorManagerSimple {
 //            }
 //        }
 //
-        return s;
+        return selected;
     }
 }
